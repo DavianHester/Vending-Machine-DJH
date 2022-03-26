7bd19dab0062bf5639dec8abbe2a844205b50b36
Davian Hester <davianhester@yahoo.com>
2022-03-26 15:41:46 -0400

    Person Class added as well as list command

--- src/main/java/com/hestervending/vendingmachine/VendingMachineApplication.java
+++ src/main/java/com/hestervending/vendingmachine/VendingMachineApplication.java
@@ -1,4 +1,4 @@
-package com.hestervending.vendingmachine;
+package src.main.java.com.hestervending.vendingmachine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

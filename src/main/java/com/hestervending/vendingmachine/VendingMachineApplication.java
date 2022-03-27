999026b79374b2b42a5b2a07aa558bfc1c665296
Davian Hester <davianhester@yahoo.com>
2022-03-27 12:31:22 -0400

    Object Mapper to map JSON data to java objects

--- src/main/java/com/hestervending/vendingmachine/VendingMachineApplication.java
+++ src/main/java/com/hestervending/vendingmachine/VendingMachineApplication.java
@@ -1,13 +1,30 @@
-package src.main.java.com.hestervending.vendingmachine;
+package com.hestervending.vendingmachine;
 
 import org.springframework.boot.SpringApplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+
 @SpringBootApplication
-public class VendingMachineApplication {
+public class VendingMachineApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendingMachineApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws IOException {
+		// TODO Auto-generated method stub
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		Item item = objectMapper.readValue(new File("input.json"), Item.class);
+		
+		System.out.println(item);
+		
 	}
 
 }

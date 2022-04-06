2ee2018054b908ce3d1fc941e0d66e1ad631d350
Davian Hester <davianhester@yahoo.com>
2022-04-05 21:34:16 -0400

    Item and Person classes added along with Object mapper to map json file

---
+++ src/main/java/com/hestervending/vendingmachine/myCommands.java
@@ -0,0 +1,31 @@
+package com.hestervending.vendingmachine;
+
+import org.springframework.shell.standard.ShellMethod;
+
+import java.util.ArrayList;
+
+import org.springframework.shell.standard.ShellComponent;
+
+@ShellComponent
+public class myCommands {
+
+    @ShellMethod("Add two integers together.")
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    @ShellMethod("Add two integers together.")
+    public String list1() {
+        Item myItem = new Item();
+        myItem.name = "M&M";
+        return myItem.name;
+    }
+    @ShellMethod("List all people.")
+    public ArrayList<Person> list() {
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Person("Ryan", 45));
+        people.add(new Person("Logan", 64));
+        people.add(new Person("Maxwell", 85));
+        return people;
+    }
+}
